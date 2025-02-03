97a56bb0381d3435672d532ecf781a9d72993428
fabien-gigante <fabien.gigante@free.fr>
2025-02-03 20:02:30 +0100

    fix persistency

--- src/main/java/com/fabien_gigante/mixin/TeamMixin.java
+++ src/main/java/com/fabien_gigante/mixin/TeamMixin.java
@@ -1,15 +1,29 @@
 package com.fabien_gigante.mixin;
 
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import net.minecraft.scoreboard.Scoreboard;
 import net.minecraft.scoreboard.Team;
 import net.minecraft.inventory.EnderChestInventory;
+import net.minecraft.inventory.Inventory;
 
 import com.fabien_gigante.IEnderChestHolder;
 
 @Mixin(Team.class)
 public class TeamMixin implements IEnderChestHolder {
+	@Shadow @Final private Scoreboard scoreboard;
 	EnderChestInventory enderChestInventory = new EnderChestInventory();
+
+	@Inject(method="<init>", at=@At(value="TAIL"))
+	private void init(Scoreboard scoreboard, String name, CallbackInfo ci) {
+		enderChestInventory.addListener((Inventory sender) -> { scoreboard.updateScoreboardTeam((Team)(Object)this); });
+	}
+
 	@Override
 	public EnderChestInventory getEnderChestInventory() { return this.enderChestInventory; }
 }
