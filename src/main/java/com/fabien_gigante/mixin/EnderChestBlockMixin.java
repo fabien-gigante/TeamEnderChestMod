3db4863b2b2b4ccb7b7a276bd6fb5620e846f2be
fabien-gigante <fabien.gigante@free.fr>
2025-10-26 22:10:32 +0100

    minor change

--- src/main/java/com/fabien_gigante/mixin/EnderChestBlockMixin.java
+++ src/main/java/com/fabien_gigante/mixin/EnderChestBlockMixin.java
@@ -23,19 +23,18 @@ import com.fabien_gigante.TeamEnderChestMod;
 
 @Mixin(EnderChestBlock.class)
 public abstract class EnderChestBlockMixin {
-    @Shadow @Final
-    private static Text CONTAINER_NAME;
+    @Shadow @Final private static Text CONTAINER_NAME;
 
     @Redirect(method = "onUse", at = @At(value = "FIELD", target = "Lnet/minecraft/block/EnderChestBlock;CONTAINER_NAME:Lnet/minecraft/text/Text;"))
     private Text getContainerName(BlockState state, World world, BlockPos pos, PlayerEntity player, BlockHitResult hit) {
         if (!(player.getScoreboardTeam() instanceof IEnderChestHolder)) return CONTAINER_NAME;
-        return Text.empty().append(CONTAINER_NAME).append(" ").append(player.getScoreboardTeam().getFormattedName());
+        return Text.translatable("%s %s", CONTAINER_NAME, player.getScoreboardTeam().getFormattedName());
     }
 
     @Redirect(method = "method_55773", at = @At(value = "INVOKE", target = "Lnet/minecraft/screen/GenericContainerScreenHandler;createGeneric9x3(ILnet/minecraft/entity/player/PlayerInventory;Lnet/minecraft/inventory/Inventory;)Lnet/minecraft/screen/GenericContainerScreenHandler;"))
     private static GenericContainerScreenHandler createScreenHandler(int syncId, PlayerInventory playerInventory, Inventory inventory) {
         if (playerInventory.player.getScoreboardTeam() instanceof IEnderChestHolder team)
             return new GenericContainerScreenHandler(TeamEnderChestMod.SCREEN_HANDLER_TYPE, syncId, playerInventory, new DoubleInventory(inventory, team.getEnderChestInventory()), 6);
-        else return  GenericContainerScreenHandler.createGeneric9x3(syncId, playerInventory, inventory);
+        else return GenericContainerScreenHandler.createGeneric9x3(syncId, playerInventory, inventory);
     }
 }
